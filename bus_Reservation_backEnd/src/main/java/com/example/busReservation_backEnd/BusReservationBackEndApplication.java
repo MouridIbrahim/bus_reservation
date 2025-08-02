20a34a14d489eb5be08f8afade808b31f75aaa2d
MouridIbrahim <workmourid@gmail.com>
2025-08-02 23:58:46 +0800

    first-commit

---
+++ bus_Reservation_backEnd/src/main/java/com/example/busReservation_backEnd/BusReservationBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.busReservation_backEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusReservationBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusReservationBackEndApplication.class, args);
+	}
+
+}
---
+++ bus_Reservation_backEnd/src/test/java/com/example/busReservation_backEnd/BusReservationBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busReservation_backEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusReservationBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
